9713f6cd09f305a7b7361f975fa82ce8ce11f09a
AWolf81 <awolf2904@gmail.com>
2018-08-11 11:39:10 +0200

    added project related controls.

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -12,11 +12,15 @@ import MainContentWrapper from '../MainContentWrapper';
 import Heading from '../Heading';
 import PixelShifter from '../PixelShifter';
 import Spacer from '../Spacer';
+import Button from '../Button';
 import DevelopmentServerPane from '../DevelopmentServerPane';
 import TaskRunnerPane from '../TaskRunnerPane';
 import DependencyManagementPane from '../DependencyManagementPane';
 
 import type { Project } from '../../types';
+
+const launchEditor = window.require('react-dev-utils/launchEditor');
+const { shell } = window.require('electron');
 
 type Props = {
   project: Project,
@@ -27,6 +31,18 @@ type Props = {
 };
 
 class ProjectPage extends Component<Props> {
+  openIDE = () => {
+    const { path } = this.props.project;
+    launchEditor(path, 1, 1);
+  };
+
+  openFolder = () => {
+    const { path } = this.props.project;
+    // Show a folder in the file manager
+    // shell.showItemInFolder(path); // shows parent directory
+    shell.openItem(path);
+  };
+
   componentDidMount() {
     window.scroll({
       top: 0,
@@ -75,7 +91,14 @@ class ProjectPage extends Component<Props> {
               {project.name}
             </Heading>
           </PixelShifter>
-
+          <ProjectActionBar>
+            <ActionButton onClick={this.openFolder} size="small">
+              Open in explorer
+            </ActionButton>
+            <ActionButton onClick={this.openIDE} size="small">
+              Open IDE
+            </ActionButton>
+          </ProjectActionBar>
           <Spacer size={30} />
           <DevelopmentServerPane leftSideWidth={300} />
 
@@ -95,6 +118,18 @@ class ProjectPage extends Component<Props> {
     );
   }
 }
+
+const FlexRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ProjectActionBar = styled.div``;
+
+const ActionButton = styled(Button)`
+  margin-right: 0.5em;
+`;
 
 const fadeIn = keyframes`
   from { opacity: 0.5 }
